f1e931194febce569a8689902245c3f7ca70cf78
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-06-08 16:32:23 +0200

    added html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,15 @@ const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
-    publicPath: "dist/", // in Webpack 5 this is automatic, you don't need this line
+    // publicPath: "dist/", // in Webpack 5 this is automatic, you don't need this line
+    publicPath: "",
   },
   mode: "none",
   module: {
@@ -56,5 +58,6 @@ module.exports = {
         path.join(process.cwd(), "build/**/*"),
       ],
     }),
+    new HtmlWebpackPlugin(),
   ],
 };
