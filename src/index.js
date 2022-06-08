864e27c9e3f0451a1b4a6e564e4c366c2bf8ce4e
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-06-08 16:13:15 +0200

    Added heading component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import Heading from "./components/heading/heading";
 import HelloWorldButton from "./components/hello-world-button/hello-world-button";
 
+const heading = new Heading();
 const helloWorldButton = new HelloWorldButton();
+
+heading.render();
 helloWorldButton.render();
