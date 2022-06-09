22f3723a8c964858b46671e66234547a14be0a8a
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-06-09 11:58:33 +0200

    added webpack devServer

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import Heading from "./components/heading/heading";
 import HelloWorldButton from "./components/hello-world-button/hello-world-button";
 
 const heading = new Heading();
+const heading2 = new Heading();
 const helloWorldButton = new HelloWorldButton();
 
 heading.render();
+heading2.render();
 helloWorldButton.render();
 
 if (process.env.NODE_ENV === "production") {
