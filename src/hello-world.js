c72cde0e9d0804125b0be5950967fc74fd39f77a
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-06-09 13:45:00 +0200

    added react and minSize in optimization

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,6 +1,7 @@
 import Heading from "./components/heading/heading";
 import HelloWorldButton from "./components/hello-world-button/hello-world-button";
 import _ from "lodash";
+import React from "react";
 
 const heading = new Heading();
 heading.render(_.upperFirst("hello world"));
